edc4ed5d4f29dc4d48435331f8b7f8a61c04caf8
wilsonng21 <wilsongch92@gmail.com>
2020-11-22 16:25:50 -0500

    .env configured

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import '../App.css';
 import axios from 'axios';
 
@@ -7,17 +7,26 @@ import GifList from './GifList';
 
 function App() {
 
+  const [data, setData] = useState([])
+
+  // useEffect(() => {
+  //   axios(`http://api.giphy.com/v1/gifs/search?q=${query}&limit=24&api_key=YOUR_API_KEY`)
+  // })
+
   return (
     <>
+
       <div className="main-header">
         <div className="inner">
           <h1 className="main-title">GifSearch</h1>
           <SearchForm />
         </div>
       </div>
-      <div className="main-content">
 
+      <div className="main-content">
+        <GifList data={data} />
       </div>
+
     </>
   );
 }
